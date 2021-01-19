4d61d0950e1f622b3083bbc01440664a1d57d7f3
David Dickson <davidick080@ed.amdsb.ca>
2021-01-19 15:58:07 -0500

    Add project files.

---
+++ ice crossing interactive multchoice story/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace ice_crossing_interactive_multchoice_story
+{
+    partial class backGround
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.options1 = new System.Windows.Forms.Label();
+            this.option2 = new System.Windows.Forms.Label();
+            this.option3 = new System.Windows.Forms.Label();
+            this.option4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // options1
+            // 
+            this.options1.BackColor = System.Drawing.Color.Transparent;
+            this.options1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.options1.Location = new System.Drawing.Point(1, 614);
+            this.options1.Name = "options1";
+            this.options1.Size = new System.Drawing.Size(789, 86);
+            this.options1.TabIndex = 0;
+            this.options1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // option2
+            // 
+            this.option2.BackColor = System.Drawing.Color.Transparent;
+            this.option2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option2.Location = new System.Drawing.Point(1, 536);
+            this.option2.Name = "option2";
+            this.option2.Size = new System.Drawing.Size(789, 78);
+            this.option2.TabIndex = 1;
+            this.option2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // option3
+            // 
+            this.option3.BackColor = System.Drawing.Color.Transparent;
+            this.option3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option3.Location = new System.Drawing.Point(789, 548);
+            this.option3.Name = "option3";
+            this.option3.Size = new System.Drawing.Size(666, 66);
+            this.option3.TabIndex = 2;
+            this.option3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // option4
+            // 
+            this.option4.BackColor = System.Drawing.Color.Transparent;
+            this.option4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option4.Location = new System.Drawing.Point(789, 614);
+            this.option4.Name = "option4";
+            this.option4.Size = new System.Drawing.Size(666, 86);
+            this.option4.TabIndex = 3;
+            this.option4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // backGround
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ice_crossing_interactive_multchoice_story.Properties.Resources.Frozen_lake;
+            this.ClientSize = new System.Drawing.Size(1455, 697);
+            this.Controls.Add(this.option4);
+            this.Controls.Add(this.option3);
+            this.Controls.Add(this.option2);
+            this.Controls.Add(this.options1);
+            this.Name = "backGround";
+            this.Text = "backGround";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label options1;
+        private System.Windows.Forms.Label option2;
+        private System.Windows.Forms.Label option3;
+        private System.Windows.Forms.Label option4;
+    }
+}
+
---
+++ ice crossing interactive multchoice story/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+namespace ice_crossing_interactive_multchoice_story
+{//Code creator:David Dickson 
+    // Start date of development:Jan 19,2021
+    //to do list (must fallow in order )
+    //-Get the rough code out 
+    //-Get the key binds to react & head to the right scene 
+    //-polish
+    //bugs list
+    //-
+    //-
+    //-
+    public partial class backGround : Form
+    {//scene count
+        int scene = 0;
+        public backGround()
+        {
+            InitializeComponent();
+        }
+
+        private void backGround_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs m)
+        {
+            //M keybind 
+            if (m.KeyCode == Keys.M)
+            {
+                //intro scene
+                if (scene == 0)
+                {
+                    scene = 4;
+                }
+
+                else if (scene == 1)
+                {
+                    scene = 8;
+                }
+                else if (scene == 2)
+                {
+                    scene = 12;
+                }
+                else if (scene == 3)
+                {
+                    scene = 15;
+                }
+
+                else if (scene == 4)
+                {
+                    scene = 18;
+                }
+                //neccerary keybind to be pressed to go to winning screen
+                else if (scene == 8)
+                {
+                    scene = 20;
+                }
+                else if (scene == 10)
+                {
+                    scene = 20;
+                }
+                else if (scene == 15)
+                {
+                    scene = 20;
+                }
+                else if (scene == 18)
+                {
+                    scene = 20;
+                }
+                //retry applications
+                else if (scene == 19)
+                {
+                    scene = 0;
+                }
+                else if (scene == 20)
+                {
+                    scene = 0;
+                }
+
+            }
+            //this is for the B keybind
+            if (m.KeyCode == Keys.B)
+
+            {
+                //intro scene
+                if (scene == 0)
+                {
+                    scene = 2;
+                }
+
+                else if (scene == 1)
+                {
+                    scene = 6;
+                }
+                else if (scene == 2)
+                {
+                    scene = 10;
+                }
+                else if (scene == 3)
+                {
+                    scene = 13;
+                }
+
+                else if (scene == 4)
+                {
+                    scene = 16;
+                }
+                //n Keybind 
+                if (m.KeyCode == Keys.N)
+                {
+                    //intro scene
+                    if (scene == 0)
+                    {
+                        scene = 3;
+                    }
+                    else if (scene == 1)
+                    {
+                        scene = 7;
+                    }
+                    else if (scene == 2)
+                    {
+                        scene = 11;
+                    }
+                    else if (scene == 3)
+                    {
+                        scene = 14;
+                    }
+                    else if (scene == 4)
+                    {
+                        scene = 17;
+                    }
+                    //scenes that take to exit application 
+                    else if (scene == 19)
+                    {
+                        scene = 99;
+                    }
+                    else if (scene == 20)
+                    {
+                        scene = 99;
+                    }
+                    //exit application
+                    else if (scene == 99)
+                    {
+                        Application.Exit();
+                    }
+                    //This is for the v keybind 
+                    if (m.KeyCode == Keys.V)
+                    {
+                        if (scene == 0)
+                        {
+                            scene = 1;
+                        }
+                        else if (scene == 1)
+                        {
+                            scene = 5;
+                        }
+                        switch (scene)
+                        {
+                            //
+                            case 0:  //start scene  
+
+
+                                break;
+                            case 1:
+
+
+                                break;
+                            case 2:
+
+                                break;
+                            case 3:
+
+
+                                break;
+                            case 4:
+
+                                break;
+                            case 5:
+
+                                break;
+                            case 6:
+
+                                break;
+                            case 7:
+
+                                break;
+                            case 8:
+
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
